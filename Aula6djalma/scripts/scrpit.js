7f7506e8dc383a927a142af9a1d3dff85b132419
SabrinaJennifer <56978550+SabrinaJennifer@users.noreply.github.com>
2022-09-21 21:34:03 -0300

    Add files via upload

---
+++ Aula6djalma/scripts/scrpit.js
@@ -0,0 +1,6 @@
+function somar(){
+    var valor1= document.getElementById("valor1").value;
+    var valor2=document.getElementById("valor2").value;
+    var somar= parseInt(valor1)+parseInt(valor2);
+    alert(somar);
+}
